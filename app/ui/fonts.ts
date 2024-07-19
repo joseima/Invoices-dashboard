22e9453695e6fca0e5e89495161ff9df9bc5dd4a
joseima <joseima17@gmail.com>
2024-07-19 11:12:29 +0200

    lusitana font imported

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,5 @@
-import { Inter, Montserrat } from 'next/font/google'
+import { Inter, Montserrat, Lusitana } from 'next/font/google'
 
 export const montserrat = Montserrat({subsets: ['latin'], weight: '400'})
 export const inter = Inter({subsets: ['latin'], weight: ['400', '700']})
+export const lusitana = Inter({subsets: ['latin'], weight: ['400', '700']})
